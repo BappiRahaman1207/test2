5e4e3307f7c7dc9cdaab77d1580b9a92a829dfe8
BappiRahaman1207 <coolbappi8888@gmail.com>
2019-09-03 18:21:42 +0600

    Ignor+MyApps

---
+++ MyApps/MyApps/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Information\n");
+
+            Console.Write("Enter your Name: ");
+            string Name=Console.ReadLine();
+
+            Console.Write("Enter your Age: ");
+            string Age = Console.ReadLine();
+
+            Console.Write("Enter your Address: ");
+            string Address = Console.ReadLine();
+
+
+            Console.WriteLine("\n Information \n");
+
+            Console.WriteLine("Name : " + Name);
+            Console.WriteLine("Age : " + Age);
+            Console.WriteLine("Address : " + Address);
+
+            Console.ReadKey();
+
+        }
+    }
+}
